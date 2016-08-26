8f79773e13316fbcc5b70b7dec747bdc52358147
Gereon Kaiping <anaphory@yahoo.de>
2016-08-26 14:28:40 +0200

    Use higher zoom on maps

--- lexibank/maps.py
+++ lexibank/maps.py
@@ -1,18 +1,24 @@
 from __future__ import unicode_literals
 
-from clld.web.maps import ParameterMap, Map
+from clld.web.maps import Map, ParameterMap, LanguageMap
 
 
 class ConceptMap(ParameterMap):
     def get_options(self):
-        return {'icon_size': 15}
+        return {'max_zoom': 12, 'icon_size': 15}
 
 
 class LanguagesMap(Map):
     def get_options(self):
-        return {'icon_size': 15}
+        return {'max_zoom': 12, 'icon_size': 20}
+
+
+class ZoomLanguageMap(LanguageMap):
+    def get_options(self):
+        return {'max_zoom': 12, 'icon_size': 20}
 
 
 def includeme(config):
     config.register_map('parameter', ConceptMap)
+    config.register_map('language', ZoomLanguageMap)
     config.register_map('languages', LanguagesMap)
