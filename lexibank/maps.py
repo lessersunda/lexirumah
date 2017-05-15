695e0ef10308921ad6f94b6f65c0774eb0b408c9
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-05-15 10:05:31 +0200

    Improve maps

--- lexibank/maps.py
+++ lexibank/maps.py
@@ -1,16 +1,33 @@
-from clld.web.maps import Map, ParameterMap, LanguageMap
+from clld.web.maps import Map, ParameterMap, LanguageMap, SelectedLanguagesMap
+from clld.web.adapters.geojson import get_lonlat
 
 class HighZoomParameterMap (ParameterMap):
     def get_options(self):
-        return {'max_zoom': 10, 'icon_size': 20}
+        return {'info_query': {'parameter': self.ctx.pk}, 'hash': True,
+                'max_zoom': 11, 'icon_size': 20}
 
 class HighZoomLanguageMap (LanguageMap):
     def get_options(self):
-        return {'max_zoom': 10, 'icon_size': 20}
+        return {
+            'center': list(reversed(get_lonlat(self.ctx) or [0, 0])),
+            'zoom': 9,
+            'no_popup': True,
+            'no_link': True,
+            'sidebar': True,
+            'max_zoom': 11,
+            'icon_size': 20}
     
 class HighZoomMap (Map):
     def get_options(self):
-        return {'max_zoom': 10, 'icon_size': 20}
+        return {'max_zoom': 11, 'icon_size': 20}
+
+
+class HighZoomSelectedLanguagesMap (SelectedLanguagesMap):
+    def get_options(self):
+        options = SelectedLanguagesMap.get_options(self)
+        options.update({'max_zoom': 11, 'icon_size': 20})
+        return options
+
     
 def includeme(config):
     config.register_map('parameter', HighZoomParameterMap)
