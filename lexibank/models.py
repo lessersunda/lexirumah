661e0a8e5bf0b509ce05fb72031360ba4937681b
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-11-10 11:47:20 +0100

    Fix geography things in Lexibank display

--- lexibank/models.py
+++ lexibank/models.py
@@ -92,6 +92,7 @@ class Concept(CustomModelMixin, Parameter):
 @implementer(interfaces.ILanguage)
 class LexibankLanguage(CustomModelMixin, Language, HasFamilyMixin):
     pk = Column(Integer, ForeignKey('language.pk'), primary_key=True)
+    region = Column(Unicode)
     level = Column(Unicode)
     culture = Column(Unicode)
     comment = Column(Unicode)
