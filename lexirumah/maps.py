cc0aa6bb0f564edfb72a83698fdb434ee1c5a702
Gereon Kaiping <Gereon.Kaiping@deutschebahn.com>
2025-05-14 23:29:18 +0200

    Set a better max zoom level

--- lexirumah/maps.py
+++ lexirumah/maps.py
@@ -3,14 +3,14 @@ from clld.web.maps import ParameterMap, Map
 
 class ConceptMap(ParameterMap):
     def get_options(self):
-        return {'icon_size': 15}
+        return {"icon_size": 15, "max_zoom": 13}
 
 
 class LanguagesMap(Map):
     def get_options(self):
-        return {'icon_size': 15}
+        return {"icon_size": 15, "max_zoom": 13}
 
 
 def includeme(config):
-    config.register_map('parameter', ConceptMap)
-    config.register_map('languages', LanguagesMap)
+    config.register_map("parameter", ConceptMap)
+    config.register_map("languages", LanguagesMap)
